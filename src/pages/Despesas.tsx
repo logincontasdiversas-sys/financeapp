ab2d100bacd878e24b833ea1f0f895fcd13fa705
logincontasdiversas-sys <logincontasdiversas-sys@users.noreply.github.com>
2025-09-22 00:39:42 -0300

    tweak: subir timeline mais um pouco (top 2.6rem -> 2.45rem)

--- src/pages/Despesas.tsx
+++ src/pages/Despesas.tsx
@@ -1526,7 +1526,7 @@ const Despesas = () => {
                     {despesasOfDate.length > 1 && (
                       <div className="pointer-events-none absolute inset-0 grid grid-cols-12 gap-2">
                         <div className="col-start-3 relative justify-self-center">
-                          <div className="absolute left-1/2 -translate-x-1/2 w-[3px] bg-orange-500 rounded-full" style={{ top: '2.6rem', height: `${(despesasOfDate.length - 1) * 56}px` }} />
+                          <div className="absolute left-1/2 -translate-x-1/2 w-[3px] bg-orange-500 rounded-full" style={{ top: '2.45rem', height: `${(despesasOfDate.length - 1) * 56}px` }} />
                         </div>
                       </div>
                     )}
